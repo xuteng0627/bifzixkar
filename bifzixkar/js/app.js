d63cedd98054d69e7bf250e061b9cd27ef10a7e3
xuteng0627 <50856732+xuteng0627@users.noreply.github.com>
2026-08-31 10:55:29 +0800

    Update app.js via editor

--- bifzixkar/js/app.js
+++ bifzixkar/js/app.js
@@ -1011,12 +1011,6 @@
           "price": "RMB 398.00",
           "image": "images/products/home3.jpg",
           "link": "https://item.taobao.com/item.htm?id=929647215619"
-        },
-        {
-          "name": "",
-          "price": "",
-          "image": "d50f2295-6d5c-450c-9857-838ecf969710.png",
-          "link": ""
         }
       ]
     },
