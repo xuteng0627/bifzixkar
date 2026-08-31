c0e70ffcb7f5583a0cbc357eb1c181b01c3216b7
xuteng0627 <50856732+xuteng0627@users.noreply.github.com>
2026-08-31 14:10:11 +0800

    Update app.js via editor

--- bifzixkar/js/app.js
+++ bifzixkar/js/app.js
@@ -473,12 +473,6 @@
           "price": "RMB 398.00",
           "image": "images/products/home3.jpg",
           "link": "https://item.taobao.com/item.htm?id=929647215619"
-        },
-        {
-          "name": "测试",
-          "image": "牛排.png",
-          "price": "59",
-          "link": "https://item.taobao.com/item.htm?id=624528913455"
         }
       ]
     },
@@ -1017,12 +1011,6 @@
           "price": "RMB 398.00",
           "image": "images/products/home3.jpg",
           "link": "https://item.taobao.com/item.htm?id=929647215619"
-        },
-        {
-          "name": "测试2",
-          "price": "95",
-          "image": "牛排.png",
-          "link": "https://item.taobao.com/item.htm?id=624528913455"
         }
       ]
     },
